126190b4ea586fb5b01ceda1bd7d74caa6b97e5a
naynyizin <naynyizin@gmail.com>
2022-09-06 19:16:41 +0630

    first commit

---
+++ l1accordion/app.js
@@ -0,0 +1,31 @@
+var getacctitles = document.getElementsByClassName("acctitle");
+// console.log(getacctitles); // HTML Collection
+var getacccontents = document.querySelectorAll(".accontent");
+// console.log(getacccontent); // Node List
+
+for(var x = 0 ; x < getacctitles.length; x++){
+    // console.log(x);
+
+    getacctitles[x].addEventListener('click',function(e){
+        // console.log(e.target);
+        // console.log(this);
+
+        this.classList.toggle("active");
+        var getcontent = this.nextElementSibling;
+        // console.log(getcontent);
+
+        if(getcontent.style.height){
+            getcontent.style.height= null; // beware can't set 0
+        }else{
+            // console.log(getcontent.scrollHeight);
+            getcontent.style.height= getcontent.scrollHeight + "px";
+
+            // getcontent.style.height="50px";
+        }
+
+    })
+
+    if(getacctitles[x].classList.contains("active")){
+        getacccontents[x].style.height = getacccontents[x].scrollHeight+ 'px';
+    }
+}
